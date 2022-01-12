bc9705a6faad6b7f6737c92a0223a07c5986f80a
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-12 15:01:03 +0100

    dfgdfg

--- restore.mysql.sh
+++ restore.mysql.sh
@@ -2,5 +2,5 @@
 docker-compose down
 docker volume rm docker-common_mysql
 docker volume create docker-common_mysql
-docker run --rm -it -v "mysql_backup:/from" -v "docker-common_mysql:/to" alpine ash -c "cd /from ; cp -av . /to"
+docker run --rm -v "mysql_backup:/from" -v "docker-common_mysql:/to" alpine ash -c "cd /from ; cp -av . /to"
 docker-compose up -d
